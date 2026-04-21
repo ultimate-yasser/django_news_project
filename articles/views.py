55450f010e69c47ece173bd01a39e6db8c2f9eeb
ultimate-yasser <yasser75894@gmail.com>
2026-04-21 03:15:04 +0200

    Articles CRUD operations

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,12 @@
-from django.views.generic import ListView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+    CreateView,
+)
 from .models import Article
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -7,3 +14,33 @@ from .models import Article
 class ArticleListView(ListView):
     model = Article
     template_name = "article_list.html"
+
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = "article_detail.html"
+
+
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = "article_edit.html"
+    fields = [
+        "title",
+        "body",
+    ]
+
+
+class ArticleDeleteView(DeleteView):
+    model = Article
+    template_name = "article_delete.html"
+    success_url = reverse_lazy("article_list")
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    template_name = "article_new.html"
+    fields = [
+        "title",
+        "body",
+        "author",
+    ]
